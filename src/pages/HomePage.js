ba89ebccc8728e92240601f9065b2009d9aee500
Listiarso Wastuargo <hallucinogenplus@gmail.com>
2016-02-06 18:39:21 -0800

    Initial RN Commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
